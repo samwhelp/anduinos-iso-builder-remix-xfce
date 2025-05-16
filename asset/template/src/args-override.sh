adcd19485b79fdcdeca1a86386f0f2e94a00a47f
samwhelp <samwhelp@users.noreply.github.com>
2025-05-16 17:29:15 +0800

    fix

--- asset/template/src/args-override.sh
+++ asset/template/src/args-override.sh
@@ -1,2 +1,11 @@
 
+
+##
+## ## Args / Override
+##
+
+export LIVE_UBUNTU_MIRROR="http://archive.ubuntu.com/ubuntu/"
+
 export BUILD_UBUNTU_MIRROR="http://tw.archive.ubuntu.com/ubuntu/"
+
+export TIMEZONE="Asia/Taipei"
