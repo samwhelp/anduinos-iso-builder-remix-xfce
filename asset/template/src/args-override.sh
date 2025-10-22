3555284601c400aab4ab1660e1d94f414d7a1f16
samwhelp <samwhelp@users.noreply.github.com>
2025-10-22 21:38:32 +0800

    fix

--- asset/template/src/args-override.sh
+++ asset/template/src/args-override.sh
@@ -9,3 +9,5 @@ export LIVE_UBUNTU_MIRROR="http://archive.ubuntu.com/ubuntu/"
 export BUILD_UBUNTU_MIRROR="http://tw.archive.ubuntu.com/ubuntu/"
 
 export TIMEZONE="Asia/Taipei"
+
+export INSTALL_MODIFIED_SOFTWARE_PROPERTIES_GTK="false"
